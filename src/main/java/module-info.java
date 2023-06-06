2d96c6da084162d067b51eb0275a1498cdc984e6
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-06-06 10:48:18 +0200

    added gson

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module at.htlsteyr.quizapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
-
+    opens at.htlsteyr.quizapp.Model to com.google.gson;
     opens at.htlsteyr.quizapp to javafx.fxml;
     exports at.htlsteyr.quizapp;
     exports at.htlsteyr.quizapp.Controller;
