73a55d121c2e820eb7727a2763884cd183d4409b
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-06-13 22:31:38 +0200

    added addQuizToList

--- src/main/java/at/htlsteyr/quizapp/Controller/SelectionViewController.java
+++ src/main/java/at/htlsteyr/quizapp/Controller/SelectionViewController.java
@@ -1,5 +1,17 @@
+/*-----------------------------------------------------------------------------
+ *              Hoehere Technische Bundeslehranstalt STEYR
+ *----------------------------------------------------------------------------*/
+/**
+ * Kurzbeschreibung
+ *
+ * @author : Dino Kupinic
+ * @date : 13.6.2023
+ * @details Controller to handle all controls for the quiz selection view
+ */
+
 package at.htlsteyr.quizapp.Controller;
 
+import at.htlsteyr.quizapp.Model.JsonHandler;
 import at.htlsteyr.quizapp.Model.Player;
 import at.htlsteyr.quizapp.Model.Quiz;
 import javafx.fxml.FXML;
@@ -7,6 +19,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+
+import java.util.ArrayList;
 
 public class SelectionViewController {
     @FXML
@@ -22,7 +36,35 @@ public class SelectionViewController {
     @FXML
     private TableColumn<Player, String> nameCol;
     @FXML
-    private ListView<Quiz> quizesList;
+    private ListView<String> quizesList;
     @FXML
     private Button playButton;
+
+    public void initialize() {
+        addQuizToList();
+    }
+
+    public void displayTopPlayers() {
+
+    }
+
+    public void displayLeaderboard() {
+
+    }
+
+    /**
+     * Adds all quiz to the list view
+     */
+    public void addQuizToList() {
+        JsonHandler jsonHandler = new JsonHandler();
+        ArrayList<Quiz> quizArrayList = null;
+        if (jsonHandler.isDataJsonValid()) {
+            quizArrayList = jsonHandler.getAllQuizes();
+        }
+        if (quizArrayList != null) {
+            for (Quiz q : quizArrayList) {
+                quizesList.getItems().add(q.getName());
+            }
+        }
+    }
 }
