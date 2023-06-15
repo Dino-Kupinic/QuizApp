0d1d69d87c887cca89c2bc6bb97e23d8705276e0
dsamhabe <dsamhabe@htl-steyr.ac.at>
2023-06-15 12:31:56 +0200

    Change Design of background, question label (font) and buttons.

--- src/main/java/at/htlsteyr/quizapp/Controller/QuizgameController.java
+++ src/main/java/at/htlsteyr/quizapp/Controller/QuizgameController.java
@@ -1,15 +1,29 @@
 package at.htlsteyr.quizapp.Controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class QuizgameController {
     @FXML
-    public AnchorPane anchorPane;
+    private AnchorPane fourAnswerAnchorPane;
+    @FXML
+    private AnchorPane trueFalseAnchorPane;
+    @FXML
+    private Button falseBtn;
+    @FXML
+    private Button trueBtn;
+    @FXML
+    private ColumnConstraints trueFalsePane;
     @FXML
     private Button topleftBtn;
     @FXML
@@ -24,7 +38,15 @@ public class QuizgameController {
     private Label questionLbl;
 
     public void setGameStart() {
-        //Image background = new Image(System.getProperty("user.dir")+ "src/main/resources/Download.jfif");
-        //anchorPane.setBackground(new Background(new BackgroundImage(background, null,null,null,null)));
+        DropShadow shadow = new DropShadow();
+        shadow.setRadius(5.0);
+        bottomleftBtn.setEffect(shadow);
+        topleftBtn.setEffect(shadow);
+        bottomrightBtn.setEffect(shadow);
+        toprightBtn.setEffect(shadow);
+        questionLbl.setBackground(new Background(new BackgroundFill(Color.DARKGRAY, null, null)));
+        questionLbl.setText("ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        Image background = new Image("H:\\Schule\\3_Klasse\\ITP2\\QuizApp\\src\\main\\resources\\img\\ClassroomBackground.png");
+        fourAnswerAnchorPane.setBackground(new Background(new BackgroundImage(background, null,null,null,null)));
     }
 }
