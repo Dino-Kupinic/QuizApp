b3c269410a783893e7830e8307ad4bef51b839af
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-06-20 15:00:02 +0200

    Updated MainApplication.java, added filling data.json with prototype-data

--- src/main/java/at/htlsteyr/quizapp/MainApplication.java
+++ src/main/java/at/htlsteyr/quizapp/MainApplication.java
@@ -35,12 +35,12 @@
 package at.htlsteyr.quizapp;
 
 import at.htlsteyr.quizapp.Controller.StartUpController;
-import at.htlsteyr.quizapp.Model.JsonHandler;
-import at.htlsteyr.quizapp.Model.WindowManager;
+import at.htlsteyr.quizapp.Model.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainApplication extends Application {
     public static int WIDTH = 1024;
@@ -55,6 +55,21 @@ public class MainApplication extends Application {
 
         StartUpController controller = (StartUpController) mainWindow.getController();
         controller.initController();
+
+        JsonHandler jsonHandler = new JsonHandler();
+        ArrayList<Answer> answers = new ArrayList<>();
+        answers.add(new Answer("a", false));
+        answers.add(new Answer("b", true));
+        Question question = new Question("samc", answers);
+        ArrayList<Question> questions = new ArrayList<>();
+        questions.add(question);
+        ArrayList<Player> topPlayers = new ArrayList<>();
+        topPlayers.add(new Player(1, "samc", new Score(0.0), new Score(0.0)));
+        Quiz quiz = new Quiz("samc2", questions, topPlayers);
+        jsonHandler.writeQuizToJson(quiz);
+
+        jsonHandler.writePlayerToJson(new Player(1, "dino", new Score(0.0), new Score(0.0)));
+        System.out.println(jsonHandler.getAllQuizes());
 
     }
 
